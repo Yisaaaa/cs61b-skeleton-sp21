04e4c56084d6413f7f67ab38c0c83e30f6f6d8a4
Yisaaaa <ryansanisit19@gmail.com>
2022-05-19 20:11:29 +0800

    MaxArrayDeque

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -33,26 +33,37 @@ public class TestBuggyAList {
     @Test
     public void randomizedTest() {
         AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> B = new BuggyAList<>();
 
-        int N = 500;
+        int N = 5000;
         for (int i = 0; i < N; i += 1) {
             int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
                 L.addLast(randVal);
-                System.out.println("addLast(" + randVal + ")");
+                B.addLast(randVal);
+
             } else if (operationNumber == 1) {
                 // size
-                int size = L.size();
-                System.out.println("size: " + size);
+                int a = L.size();
+                int b = B.size();
+
+                assertEquals(a, b);
+
             } else if (L.size() > 0) {
                 if (operationNumber == 2) {
-                    int last = L.getLast();
-                    System.out.println("Last: " + last);
+                    int a = L.getLast();
+                    int b = B.getLast();
+
+                    assertEquals(a, b);
+
                 } else if (operationNumber == 3) {
-                    int removedLast = L.removeLast();
-                    System.out.println("Removed Last: " + removedLast);
+                    int a = L.removeLast();
+                    int b = B.removeLast();
+
+                    assertEquals(a, b);
+
                 }
             }
         }
--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,5 +1,74 @@
 package deque;
 
-public class MaxArrayDeque {
-    
+import java.lang.reflect.InaccessibleObjectException;
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+
+    Comparator<T> comparator;
+    public class Student {
+
+        int studentNumber, gradeLevel;
+        String name;
+
+        public Student(String n, int g, int s) {
+            studentNumber = s;
+            gradeLevel = g;
+            name = n;
+        }
+    }
+
+    public class compareByStudentNumber implements Comparator<Student> {
+        @Override
+        public int compare(Student student1, Student student2) {
+            if (student1.studentNumber == student2.studentNumber) {
+                return 0;
+            } else if (student1.studentNumber > student2.studentNumber) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    public MaxArrayDeque(Comparator<T> c) {
+        super();
+        comparator = c;
+    }
+
+    public T max() {
+
+        if (size() == 0) {
+            return null;
+        }
+
+        T max = get(0);
+        for (int i = 1; i < size(); i++) {
+            T current = get(i);
+            if (comparator.compare(current, max) == 1) {
+                max = current;
+            }
+        }
+        return max;
+    }
+
+    public T max(Comparator<T> c) {
+
+        if (size() == 0) {
+            return null;
+        }
+
+        T max = get(0);
+        for (int i = 1; i < size(); i++) {
+            T current = get(i);
+            if (c.compare(current, max) == 1) {
+                max = current;
+            }
+        }
+        return max;
+    }
+
+
+
+
 }
